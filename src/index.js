8789c9d19228ff34816ee523b5777358f6fa6ad1
null267 <eigenheiter@gmail.com>
2020-04-18 16:34:41 -0400

    fetch with useReducer working

--- src/index.js
+++ src/index.js
@@ -11,27 +11,59 @@ import endpoint from './endpoint';
 
 import './styles.scss';
 
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const LOADING = 'LOADING';
+const RESPONSE_COMPLETE = 'RESPONSE_COMPLETE';
+const ERROR = 'ERROR';
+
+const fetchReducer = (state, action) => {
+  if (action.type === LOADING) {
+    return {
+      result: null,
+      loading: true,
+      error: null,
+    }
+  }
+
+  if (action.type === RESPONSE_COMPLETE) {
+    return {
+      result: action.payload.response,
+      loading: false,
+      error: null,
+    }
+  }
+
+  if (action.type === ERROR) {
+    return {
+      result: null,
+      loading: false,
+      error: action.payload.error,
+    }
+  }
+
+  return state;
+}
+
   const useFetch = url => {
-    const [response, setResponse] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = React.useReducer(fetchReducer, initialState);
 
     React.useEffect(() => {
-      setLoading(true);
-      setResponse(null);
-      setError(null);
-      fetch(endpoint + '/characters')
+      dispatch({ type: LOADING });
+      fetch(url)
         .then(response => response.json())
         .then(response => {
-          setLoading(false);
-          setResponse(response);
+          dispatch({ type: RESPONSE_COMPLETE, payload: { response } })
         })
         .catch(error => {
-          setLoading(false);
-          setError(error);
+          dispatch({ type: ERROR, payload: { error } });
         });
     }, []);
-    return [response, loading, error];
+    return [state.result, state.loading, state.error];
   }
 
 const Application = () => {
